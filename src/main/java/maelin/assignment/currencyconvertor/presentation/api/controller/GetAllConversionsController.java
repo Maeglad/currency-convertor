c2dd5c41c01de28547b96d4ea560463623b45785
Julia Miklosovicova <miklosovicl@gmail.com>
2023-11-09 22:06:14 +0100

    implemented GetAllConversionsController

--- src/main/java/maelin/assignment/currencyconvertor/presentation/api/controller/GetAllConversionsController.java
+++ src/main/java/maelin/assignment/currencyconvertor/presentation/api/controller/GetAllConversionsController.java
@@ -1,29 +1,40 @@
 package maelin.assignment.currencyconvertor.presentation.api.controller;
 
 import maelin.assignment.currencyconvertor.domain.use_case.GetAllConversionsUseCase;
+import maelin.assignment.currencyconvertor.presentation.api.model.ConversionRateDTO;
 import maelin.assignment.currencyconvertor.presentation.api.request.BaseApiRequest;
 import maelin.assignment.currencyconvertor.presentation.api.response.BaseApiResponse;
+import maelin.assignment.currencyconvertor.presentation.api.response.ErrorResponse;
+import maelin.assignment.currencyconvertor.presentation.api.response.GetAllConversionsResponse;
+import maelin.assignment.currencyconvertor.presentation.api.util.EntityMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api/conversions", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GetAllConversionsController implements BaseApiController<BaseApiRequest> {
 
-    private GetAllConversionsUseCase useCase;
+    private final GetAllConversionsUseCase useCase;
+
+    private final EntityMapper entityMapper;
 
     @Autowired
-    public GetAllConversionsController(GetAllConversionsUseCase useCase) {
+    public GetAllConversionsController(GetAllConversionsUseCase useCase,
+                                       EntityMapper entityMapper) {
         this.useCase = useCase;
+        this.entityMapper = entityMapper;
     }
 
     @Override
@@ -33,6 +44,16 @@ public class GetAllConversionsController implements BaseApiController<BaseApiReq
             @RequestBody
             Optional<BaseApiRequest> request,
             BindingResult bindingResult) {
-        return null;
+        if (bindingResult.hasErrors()) {
+            String errorMessage = bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .collect(Collectors.joining("\n"));
+            return ResponseEntity.badRequest().body(new ErrorResponse(errorMessage));
+        }
+
+        List<ConversionRateDTO> results = useCase.invoke()
+                .stream().map(entityMapper::convertToDTO).toList();
+
+        return ResponseEntity.ok(new GetAllConversionsResponse(results));
     }
 }
