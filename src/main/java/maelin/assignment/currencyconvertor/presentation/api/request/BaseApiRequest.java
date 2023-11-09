c001769c6062d3d0dbd2c49a1e7db1dc4a61d6a6
Julia Miklosovicova <miklosovicl@gmail.com>
2023-11-09 13:47:36 +0100

    added api requests BaseApiRequest - abstract class that every request extends and contains common values
    ConvertCurrencyRequest - request for getting conversion between currencies

---
+++ src/main/java/maelin/assignment/currencyconvertor/presentation/api/request/BaseApiRequest.java
@@ -0,0 +1,4 @@
+package maelin.assignment.currencyconvertor.presentation.api.request;
+
+public abstract class BaseApiRequest {
+}
---
+++ src/main/java/maelin/assignment/currencyconvertor/presentation/api/request/ConvertCurrencyRequest.java
@@ -0,0 +1,18 @@
+package maelin.assignment.currencyconvertor.presentation.api.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Optional;
+
+public class ConvertCurrencyRequest extends BaseApiRequest {
+
+    @NotBlank(message = "fromCurrency field is blank")
+    public Optional<String> fromCurrency;
+
+    @NotBlank(message = "toCurrency field is blank")
+    public Optional<String> toCurrency;
+
+    @NotBlank(message = "amount field is blank")
+    public Optional<String> amount;
+
+}
