c0e7d5df3e2e4b7da9f8dc7ad494771279774faf
Julia Miklosovicova <miklosovicl@gmail.com>
2023-11-09 09:55:30 +0100

    setup project

---
+++ src/main/java/maelin/assignment/currencyconvertor/CurrencyConvertorSpringBootApplication.java
@@ -0,0 +1,15 @@
+package maelin.assignment.currencyconvertor;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConvertorSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConvertorSpringBootApplication.class, args);
+    }
+
+}
