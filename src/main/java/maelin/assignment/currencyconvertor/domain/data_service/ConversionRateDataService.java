535d451c5825ad7ac31f66d27215853db01ae82a
Julia Miklosovicova <miklosovicl@gmail.com>
2023-11-09 10:19:17 +0100

    added service annotation to data service interface

--- src/main/java/maelin/assignment/currencyconvertor/domain/data_service/ConversionRateDataService.java
+++ src/main/java/maelin/assignment/currencyconvertor/domain/data_service/ConversionRateDataService.java
@@ -3,9 +3,11 @@ package maelin.assignment.currencyconvertor.domain.data_service;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 import maelin.assignment.currencyconvertor.domain.model.ConversionRate;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface ConversionRateDataService {
 
     @Nonnull
