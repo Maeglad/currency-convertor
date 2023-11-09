67eecf2f30f0fc25fe0dda50d5f2cb44f89d602a
Julia Miklosovicova <miklosovicl@gmail.com>
2023-11-09 22:04:48 +0100

    added EntityMapper to map DTOs to entities

---
+++ src/main/java/maelin/assignment/currencyconvertor/presentation/api/util/EntityMapper.java
@@ -0,0 +1,34 @@
+package maelin.assignment.currencyconvertor.presentation.api.util;
+
+import maelin.assignment.currencyconvertor.domain.model.ConversionRate;
+import maelin.assignment.currencyconvertor.presentation.api.model.ConversionRateDTO;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EntityMapper {
+
+    ModelMapper modelMapper;
+
+    @Autowired
+    public EntityMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    private void configure() {
+        modelMapper.addMappings(new PropertyMap<ConversionRate, ConversionRateDTO>() {
+            @Override
+            protected void configure() {
+                map(source.getFromCurrency(), destination.fromCurrency);
+                map(source.getToCurrency(), destination.toCurrency);
+                map(source.getRate(), destination.conversionRate);
+            }
+        });
+    }
+
+    public ConversionRateDTO convertToDTO(ConversionRate conversionRate) {
+        return modelMapper.map(conversionRate, ConversionRateDTO.class);
+    }
+}
